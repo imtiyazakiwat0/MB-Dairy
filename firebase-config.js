88418660100089f483a581987386899aebb8179a
Imtiyaz Akiwat <imtiyazakiwat0@gmail.com>
2024-05-13 12:20:47 +0530

    seperate code for configaration

--- firebase-config.js
+++ firebase-config.js
@@ -1,22 +1,13 @@
-// import { initializeApp } from 'firebase/app';
-// import { getFirestore } from 'firebase/firestore';
-
-
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-
-const firebaseConfig = {
-    apiKey: "AIzaSyCJeE2bd_Y3l1Q19jsWrnYwttZ_SHHssaA",
-    authDomain: "mustafa-dairy.firebaseapp.com",
-    projectId: "mustafa-dairy",
-    storageBucket: "mustafa-dairy.appspot.com",
-    messagingSenderId: "727616627428",
-    appId: "1:727616627428:web:cb691fb786f77a69c342f1",
-    measurementId: "G-E13H2GC55B"
-  };
- 
-  
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
+const firebaseConfig = {
+  apiKey: "AIzaSyCJeE2bd_Y3l1Q19jsWrnYwttZ_SHHssaA",
+  authDomain: "mustafa-dairy.firebaseapp.com",
+  projectId: "mustafa-dairy",
+  storageBucket: "mustafa-dairy.appspot.com",
+  messagingSenderId: "727616627428",
+  appId: "1:727616627428:web:cb691fb786f77a69c342f1",
+  measurementId: "G-E13H2GC55B"
+};
+firebase.initializeApp(firebaseConfig);
 
-// export default db;
+const db = firebase.firestore();
--- scipt.js
+++ scipt.js
@@ -1,16 +1,4 @@
-// Initialize Firebase
-const firebaseConfig = {
-    apiKey: "AIzaSyCJeE2bd_Y3l1Q19jsWrnYwttZ_SHHssaA",
-    authDomain: "mustafa-dairy.firebaseapp.com",
-    projectId: "mustafa-dairy",
-    storageBucket: "mustafa-dairy.appspot.com",
-    messagingSenderId: "727616627428",
-    appId: "1:727616627428:web:cb691fb786f77a69c342f1",
-    measurementId: "G-E13H2GC55B"
-};
-firebase.initializeApp(firebaseConfig);
 
-const db = firebase.firestore();
 
 document.getElementById("billDate").addEventListener("change", generatePreviousDates);
 
